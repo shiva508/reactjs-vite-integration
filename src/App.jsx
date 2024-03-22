39608268d61ecd99d00fc68edc44b271de56163d
shiva508 <dasarishiva1@gmail.com>
2024-03-22 10:56:13 +0530

    basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <Navbar />
+      {/* <!-- Hero --> */}
+      <Hero title="I am the Hero" subTitle="I am the Vilon" />
+
+      {/* <!-- Developers and Employers --> */}
+      <HomeCards />
+
+      {/* <!-- Browse Jobs --> */}
+      <JobListings />
+      <section className="m-auto max-w-lg my-10 px-6">
+        <a
+          href="jobs.html"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+          View All Jobs
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </section>
     </>
   );
 }
